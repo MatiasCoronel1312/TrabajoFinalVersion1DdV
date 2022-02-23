2147a7a7560f186a8e8c5c30a20050a47a089691
Matias Coronel <97346804+MatiasCoronel1312@users.noreply.github.com>
2022-02-23 20:15:54 -0300

    Agregue animacion de Fire y reload

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,15 +7,6 @@ public class PlayerController : MonoBehaviour
 {
     [Header ("References")]
     public Camera playerCamera;
-    [Header ("General")]
-    public float gravityScale = -20f;
-    
-    [Header ("Jump")]
-    public float jumpHeight = 1.9f;
-
-    [Header ("Movement")]
-    public float walkSpeed = 6f;
-    public float runSpeed = 15f;
 
     [Header ("Rotation")]
     public float rotationSensibility = 30f;
@@ -24,52 +15,66 @@ public class PlayerController : MonoBehaviour
     Vector3 moveInput = Vector3.zero;
     Vector3 rotationinput = Vector3.zero;
     CharacterController characterController;
+    [SerializeField] private Animator PlayerShooter;
 
     private void Awake() {
+
         characterController= GetComponent<CharacterController>();
     }
     private void Update() {
+
         Look();
-        Move();
+        Fire();
+        Reload();
         
     }
-    private void Move() {
-
-        if (characterController.isGrounded) {
-
-            moveInput= new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
-            moveInput = Vector3.ClampMagnitude(moveInput, 1f);
-
-            if (Input.GetButton("Sprint")){
-
-                moveInput= transform.TransformDirection(moveInput)* runSpeed;
-
-            }
-            else {
-                moveInput= transform.TransformDirection(moveInput)* walkSpeed;
-            }
-
-
-             
-
-            if (Input.GetButtonDown("Jump"))
-            {
-                moveInput.y = Mathf.Sqrt(jumpHeight * -2f * gravityScale);
-
-             }
-
-        }
-        
-        moveInput.y += gravityScale * Time.deltaTime;
-        characterController.Move(moveInput * Time.deltaTime);
-    } 
+    
     private void Look () {
+
+
         rotationinput.x = Input.GetAxis("Mouse X") * rotationSensibility * Time.deltaTime;
+
         rotationinput.y = Input.GetAxis("Mouse Y") * rotationSensibility * Time.deltaTime;
 
+
         cameraVerticalAngle+=rotationinput.y;
+
         cameraVerticalAngle = Mathf.Clamp(cameraVerticalAngle, -70 , 70);
+
         transform.Rotate(Vector3.up * rotationinput.x);
+
         playerCamera.transform.localRotation = Quaternion.Euler(-cameraVerticalAngle, 0f ,0f); 
+
+    }
+
+    private void Fire ()
+    {
+        if(Input.GetKey(KeyCode.F))
+
+        {
+            PlayerShooter.SetBool("Fire", true);
+            
+        }
+        if(Input.GetKeyUp(KeyCode.F))
+        {
+            PlayerShooter.SetBool("Fire", false);
+            
+        }
+    }
+    private void Reload ()
+    {
+        if(Input.GetKey(KeyCode.R))
+
+        {
+            PlayerShooter.SetBool("Reload", true);
+            
+        }
+
+        if(Input.GetKeyUp(KeyCode.R))
+
+        {
+            PlayerShooter.SetBool("Reload", false);
+            
+        }
     }
 }
